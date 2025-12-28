7343f336911c4a23b1dfab754f7710c9e762eb4c
Fjjmyn <83651261+Fjjmyn@users.noreply.github.com>
2025-12-28 14:27:58 +0800

    Add files via upload

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,5 @@
 import logging
+import logging.handlers
 import datetime
 import os
 
@@ -22,47 +23,43 @@ def custom_timezone_converter(timestamp):
     return dt_time.timetuple()
 
 def setup_logging(log_file, prefix=None, level=logging.INFO):
-    """
-    配置日志记录器，使其输出到文件和控制台。
-    支持一个可选的前缀，用于标识日志来源。
-
-    每次调用都会重新配置处理器，以适应多进程环境。
-    
-    时间显示默认为 UTC+8 (北京时间)，可通过环境变量 TZ_OFFSET 修改。
-
-    :param log_file: 日志文件的路径。
-    :param prefix: (可选) 要添加到每条日志消息开头的字符串前缀。
-    :param level: 日志级别。
-    """
-    logger = logging.getLogger('my_app_logger') 
-    logger.setLevel(level)
-
-    if logger.hasHandlers():
-        logger.handlers.clear()
-
-    base_format = '%(asctime)s - %(process)d - %(levelname)s - %(message)s'
-
-    if prefix:
-        log_format = f'%(asctime)s - %(process)d - %(levelname)s - {prefix} - %(message)s'
-    else:
-        log_format = base_format
-
-    fh = logging.FileHandler(log_file)
-    fh.setLevel(level)
-
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
-
-    formatter = logging.Formatter(log_format)
-    # 设置自定义的时间转换器
-    formatter.converter = custom_timezone_converter
-
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-    
-    logger.propagate = False
-    
-    return logger
+     """
+     配置日志记录器，仅输出到控制台以节省 ephemeral storage。
+     支持一个可选的前缀，用于标识日志来源。
+ 
+     每次调用都会重新配置处理器，以适应多进程环境。
+     
+     时间显示默认为 UTC+8 (北京时间)，可通过环境变量 TZ_OFFSET 修改。
+ 
+     :param log_file: 日志文件的路径（已废弃，仅为兼容性保留）。
+     :param prefix: (可选) 要添加到每条日志消息开头的字符串前缀。
+     :param level: 日志级别。
+     """
+     logger = logging.getLogger('my_app_logger') 
+     logger.setLevel(level)
+ 
+     if logger.hasHandlers():
+         logger.handlers.clear()
+ 
+     base_format = '%(asctime)s - %(process)d - %(levelname)s - %(message)s'
+ 
+     if prefix:
+         log_format = f'%(asctime)s - %(process)d - %(levelname)s - {prefix} - %(message)s'
+     else:
+         log_format = base_format
+ 
+     # 仅输出到 stdout，不写文件（节省 ephemeral storage）
+     ch = logging.StreamHandler()
+     ch.setLevel(level)
+ 
+     formatter = logging.Formatter(log_format)
+     # 设置自定义的时间转换器
+     formatter.converter = custom_timezone_converter
+ 
+     ch.setFormatter(formatter)
+ 
+     logger.addHandler(ch)
+     
+     logger.propagate = False
+     
+     return logger
